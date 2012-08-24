67ce38e19b7db2304b9b5f1e96fa014e9e5a931a
Jallah <puncher@gmx.net>
2012-08-24 15:38:48 +0200

    Speed improvements

---
+++ WireMatchInfo/MatchMember.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace UserData
+{
+    class MatchMember
+    {
+        private Regex regex;
+        private HtmlNode playerNodes, playerLinkNode;
+        private const string regexIdPattern = @"(?<=player/).*(?=/)";
+        // private const string regexNamePattern = "(?<=Nick</td><td>).*(?=</td></tr><tr><td class=\"firstcol\">Member)";
+        private HtmlWeb htmlWeb;
+        private List<Player> players;
+
+        public MatchMember()
+        {
+            htmlWeb = new HtmlWeb();
+            regex = new Regex(regexIdPattern);
+        }
+
+        internal class Player
+        {
+            private readonly int id;
+            private readonly string eslPlayerLink;
+            private readonly string playerNick;
+
+            public Player(int id, string eslPlayerLink, string playerNick)
+            {
+                this.id = id;
+                this.eslPlayerLink = eslPlayerLink;
+                this.playerNick = playerNick;
+            }
+
+            public int Id
+            {
+                get { return id; }
+            }
+
+            public string EslPlayerLink
+            {
+                get { return eslPlayerLink; }
+            }
+
+            public string PlayerNick
+            {
+                get { return playerNick; }
+            }
+
+
+        } // end internal Player Class
+
+        public List<Player> GetMatchMembers(string matchlink)
+        {
+            HtmlAgilityPack.HtmlDocument matchDocument = htmlWeb.Load(matchlink);
+            HtmlAgilityPack.HtmlDocument playerDocument;
+       
+            playerNodes = matchDocument.GetElementbyId("votes_table");
+            players = new List<Player>();
+
+            // If there is no node with that Id, someNode will be null
+            if (playerNodes != null)
+            {
+                // Extracts all links within that node
+                IEnumerable<HtmlNode> allLinks = playerNodes.Descendants("a"); // alle links der player sammeln
+                // Outputs the href for external links
+                foreach (HtmlNode link in allLinks)
+                {
+                    
+                    // Get Playerlink
+                    string playerLink = "http://www.esl.eu" + link.Attributes["href"].Value;
+                    
+                    int playerId;
+
+                    //if the playerId within the playerLink isn't an 'int', load Player document
+                    if (!int.TryParse(Convert.ToString(regex.Match(playerLink)), out playerId))
+                    {
+                        playerDocument = htmlWeb.Load(playerLink);
+                        playerNodes = playerDocument.GetElementbyId("player_logo");
+                        playerLinkNode = playerNodes.SelectSingleNode("a"); // XPath-Syntax
+                        playerLink = playerLinkNode.Attributes["href"].Value;
+                        playerId = int.Parse(regex.Match(playerLink).ToString());
+                    }
+                    
+                    //Get PlayerNick
+                    string playerNick1 = link.InnerText.Trim();
+       
+                    //Add Player to Playerlist
+                    players.Add(new Player(playerId, playerLink, playerNick1));
+                } // end foreach
+            }//end if
+            return players;
+        } // end method
+    } // end MachMember Class
+}
---
+++ WireMatchInfo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //HtmlWeb htmlWeb = new HtmlWeb();
+            //HtmlAgilityPack.HtmlDocument document = htmlWeb.Load("http://www.esl.eu/de/css/ui/versus/match/2758197");
+
+            //string paternId = @"(?<=player/).*(?=/)";
+            //string paternName = "(?<=Nick</td><td>).*(?=</td></tr><tr><td class=\"firstcol\">Member)";
+
+            //Regex regex;
+            //HtmlNode playerNodes, playerLinkNode;
+            //Console.WriteLine("\nPlayer links and ID's\n");
+
+            //playerNodes = document.GetElementbyId("votes_table");
+
+            //// If there is no node with that Id, someNode will be null
+            //if (playerNodes != null)
+            //{
+            //    // Extracts all links within that node
+            //    IEnumerable<HtmlNode> allLinks = playerNodes.Descendants("a"); // alle links der player sammeln
+
+            //    // Outputs the href for external links
+            //    foreach (HtmlNode link in allLinks) // die linksdurchgehen und die id parsen
+            //    {
+            //        string playerLink = "http://www.esl.eu" + link.Attributes["href"].Value;
+
+            //        Console.WriteLine(playerLink);
+
+            //        document = htmlWeb.Load(playerLink);
+            //        playerNodes = document.GetElementbyId("player_logo");
+            //        playerLinkNode = playerNodes.SelectSingleNode("a"); // siehe XPath-Syntax
+            //        string playerLinkWithId = playerLinkNode.Attributes["href"].Value;
+            //        regex = new Regex(paternId);
+
+            //        string playerId = regex.Match(playerLinkWithId).ToString();
+
+            //        Console.WriteLine(playerId);
+
+                    
+            //        HtmlDocument myDoc = htmlWeb.Load(playerLink);
+            //        string html = myDoc.DocumentNode.OuterHtml;
+
+            //        regex = new Regex(paternName);
+            //        html = html.Replace("\n", "");
+            //        string uname = regex.Match(html).ToString();
+
+            //        Console.WriteLine(uname + "\n");
+
+            //        //StreamWriter myWriter = File.CreateText(@"c:\text.txt");
+            //        //myWriter.WriteLine(html);
+            //        //myWriter.Close();
+            //    }
+
+
+            //    Console.ReadKey();
+
+           // }
+
+            Console.WriteLine("Get Playerinfo ... this will take some seconds\n");
+            MatchMember mm = new MatchMember();
+            var pl = mm.GetMatchMembers("http://www.esl.eu/de/css/ui/versus/match/2758197");
+            
+            foreach (var player in pl)
+            {
+                Console.WriteLine(player.EslPlayerLink);
+                Console.WriteLine(player.Id);
+                Console.WriteLine(player.PlayerNick);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
