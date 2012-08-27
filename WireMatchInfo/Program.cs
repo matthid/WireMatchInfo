9d704e873a455aec31a53696541528811bfdced6
Matthias Dittrich <matthi.d@googlemail.com>
2012-08-27 14:32:37 +0200

    my changes

--- WireMatchInfo/Program.cs
+++ WireMatchInfo/Program.cs
@@ -16,24 +16,29 @@ namespace UserData
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Downloading Matchinfo. This will take some seconds !!\n");
-            MatchMember mm = new MatchMember();
-
-            List<MatchMember.Player> pl = mm.GetMatchMembers("http://www.esl.eu/de/css/ui/versus/match/2758197");
 
             Console.WriteLine();
 
-            foreach (var player in pl)
+            Console.WriteLine("Get Playerinfo ... this will take some seconds\n");
+            var l = new List<string>()
+                {
+                    "http://www.esl.eu/de/css/ui/versus/match/2778059",
+                    //"http://www.esl.eu/de/css/ui/versus/match/2777955",
+                    //"http://www.esl.eu/de/ui/versus/match/2758882",
+                    //"http://www.esl.eu/de/css/ui/versus/match/2758197",
+                    //"http://www.esl.eu/de/css/ui/versus/match/2754367"
+                };
+            var s = System.Diagnostics.Stopwatch.StartNew();
+            MatchMember mm = new MatchMember();
+            foreach (var link in l)
             {
-                Console.WriteLine(player.EslPlayerLink);
-                Console.WriteLine(player.Id);
-                Console.WriteLine(player.PlayerNick);
-                Console.WriteLine();
+                mm.GetMatchMembers(link); //("http://www.esl.eu/de/css/ui/versus/match/2758197");
             }
+            s.Stop();
+
+            Console.WriteLine("ElapsedTime: {0}ms\n", s.ElapsedMilliseconds);
 
             Console.ReadKey();
-
-
         }
     }
 
